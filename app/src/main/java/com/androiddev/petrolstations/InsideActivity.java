32c631548bb3a263152f4c2966d3b6d182eaf2f2
Kaavish <wetrivial@gmail.com>
2023-06-13 22:07:45 +0530

    bottom navigation added

--- app/src/main/java/com/androiddev/petrolstations/InsideActivity.java
+++ app/src/main/java/com/androiddev/petrolstations/InsideActivity.java
@@ -9,6 +9,8 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -19,10 +21,57 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import com.androiddev.petrolstations.databinding.ActivityBottomNavBinding;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+
+
+
 public class InsideActivity extends AppCompatActivity {
+
+    public class bottom_navActivity extends AppCompatActivity {
+
+        ActivityBottomNavBinding binding;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            binding = ActivityBottomNavBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+            replaceFragment(new HomeFragment());
+
+            binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        replaceFragment(new HomeFragment());
+                        break;
+                    case R.id.service:
+                        replaceFragment(new PetrolPumpsFragment());
+                        break;
+                    case R.id.speedometer:
+                        replaceFragment(new PetrolPumpsFragment());
+                        break;
+                    case R.id.notifiation:
+                        replaceFragment(new notificationFragment());
+                        break;
+                    case R.id.profile:
+                        replaceFragment(new profileFragment());
+                        break;
+                }
+
+            });
+        }
+
+        private void replaceFragment(Fragment fragment){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout,fragment);
+            fragmentTransaction.commit();
+        }
+    }
 
     Toolbar toolbar;
     NavigationView navigationView;
@@ -140,6 +189,9 @@ public class InsideActivity extends AppCompatActivity {
         });
 
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
